db758c7181a5a65927174da1f4c4e1b784544a31
Tim Burks <timburks@google.com>
2017-04-22 22:17:12 -0700

    Viewing and scrolling in files

--- goed.go
+++ goed.go
@@ -4,7 +4,9 @@ import (
 	"bufio"
 	"errors"
 	"fmt"
+	"io/ioutil"
 	"os"
+	"strings"
 	"time"
 
 	"./terminal"
@@ -25,28 +27,43 @@ const (
 	DELETE      = 1008
 )
 
-type Editor struct {
-	Reader *bufio.Reader
+type Row struct {
+	Text string
+}
 
+type Editor struct {
+	Reader     *bufio.Reader
 	ScreenRows int
 	ScreenCols int
-
-	CursorRow int
-	CursorCol int
-
-	Message string
+	EditRows   int // actual number of rows used for text editing
+	EditCols   int
+	CursorRow  int
+	CursorCol  int
+	Message    string // status message
+	Rows       []Row
+	RowOffset  int
+	ColOffset  int
 }
 
 func NewEditor() *Editor {
-	editor := &Editor{}
-	editor.Reader = bufio.NewReader(os.Stdin)
-	w, h, err := terminal.GetSize(0)
+	e := &Editor{}
+	e.Reader = bufio.NewReader(os.Stdin)
+	e.Rows = make([]Row, 0)
+	return e
+}
+
+func (e *Editor) ReadFile(path string) error {
+	b, err := ioutil.ReadFile(path)
 	if err != nil {
-		fmt.Print(err)
+		return err
 	}
-	editor.ScreenRows = h
-	editor.ScreenCols = w
-	return editor
+	s := string(b)
+	lines := strings.Split(s, "\n")
+	e.Rows = make([]Row, 0)
+	for _, line := range lines {
+		e.Rows = append(e.Rows, Row{Text: line})
+	}
+	return nil
 }
 
 func (e *Editor) ReadKey() int {
@@ -59,7 +76,7 @@ func (e *Editor) ReadKey() int {
 			time.Sleep(time.Microsecond)
 		}
 	}
-	e.Message = fmt.Sprintf(" code=%02x", b[0:n])
+	//e.Message = fmt.Sprintf(" code=%02x", b[0:n])
 	if err != nil {
 		fmt.Print(err)
 	}
@@ -109,18 +126,18 @@ func (e *Editor) ReadKey() int {
 
 func (e *Editor) ProcessKeyPress() error {
 	key := e.ReadKey()
-	e.Message += fmt.Sprintf(" key=%d", key)
+	//e.Message += fmt.Sprintf(" key=%d", key)
 
 	switch key {
 	case CTRL_Q:
 		e.Exit()
 		return errors.New("quit")
 	case PAGE_UP:
-		for times := e.ScreenRows; times > 0; times-- {
+		for times := e.EditRows; times > 0; times-- {
 			e.MoveCursor(ARROW_UP)
 		}
 	case PAGE_DOWN:
-		for times := e.ScreenRows; times > 0; times-- {
+		for times := e.EditRows; times > 0; times-- {
 			e.MoveCursor(ARROW_DOWN)
 		}
 	case HOME:
@@ -133,13 +150,40 @@ func (e *Editor) ProcessKeyPress() error {
 	return nil
 }
 
+func (e *Editor) Scroll() {
+	if e.CursorRow < e.RowOffset {
+		e.RowOffset = e.CursorRow
+	}
+	if e.CursorRow-e.RowOffset >= e.EditRows {
+		e.RowOffset = e.CursorRow - e.EditRows + 1
+	}
+	if e.CursorCol < e.ColOffset {
+		e.ColOffset = e.CursorCol
+	}
+	if e.CursorCol-e.ColOffset >= e.EditCols {
+		e.ColOffset = e.CursorCol - e.EditCols + 1
+	}
+
+	e.Message = fmt.Sprintf("(%d,%d)", e.CursorRow, e.CursorCol)
+}
+
 func (e *Editor) RefreshScreen() {
+	w, h, err := terminal.GetSize(0)
+	if err != nil {
+		fmt.Print(err)
+	}
+	e.ScreenRows = h
+	e.ScreenCols = w
+	e.EditRows = e.ScreenRows - 1
+	e.EditCols = e.ScreenCols
+
+	e.Scroll()
 	buffer := make([]byte, 0)
 	buffer = append(buffer, []byte("\x1b[?25l")...) // hide cursor
 	buffer = append(buffer, []byte("\x1b[1;1H")...) // move cursor to row 1, col 1
 	buffer = e.DrawRows(buffer)
 
-	buffer = append(buffer, []byte(fmt.Sprintf("\x1b[%d;%dH", e.CursorRow+1, e.CursorCol+1))...)
+	buffer = append(buffer, []byte(fmt.Sprintf("\x1b[%d;%dH", e.CursorRow+1-e.RowOffset, e.CursorCol+1-e.ColOffset))...)
 
 	buffer = append(buffer, []byte("\x1b[?25h")...) // show cursor
 	os.Stdout.Write(buffer)
@@ -153,23 +197,39 @@ func (e *Editor) Exit() {
 }
 
 func (e *Editor) DrawRows(buffer []byte) []byte {
-	for y := 1; y <= e.ScreenRows; y++ {
-		if y == e.ScreenRows/3 {
-			welcome := fmt.Sprintf("goed editor -- version %s", VERSION)
-			padding := (e.ScreenCols - len(welcome)) / 2
-			buffer = append(buffer, []byte("~")...)
-			for i := 1; i <= padding; i++ {
-				buffer = append(buffer, []byte(" ")...)
+	for y := 0; y < e.ScreenRows; y++ {
+		if y == e.ScreenRows-1 {
+			buffer = append(buffer, []byte(e.Message)...)
+			buffer = append(buffer, []byte("\x1b[K")...)
+		} else if (y + e.RowOffset) < len(e.Rows) {
+			line := e.Rows[y+e.RowOffset].Text
+			line = strings.Replace(line, "\t", "        ", -1)
+
+			if len(line) > e.ColOffset {
+				line = line[e.ColOffset:]
+			} else {
+				line = ""
 			}
-			buffer = append(buffer, []byte(welcome)...)
-		} else {
-			buffer = append(buffer, []byte("~")...)
-		}
-		buffer = append(buffer, []byte("\x1b[K")...)
-		if y < e.ScreenRows {
+			if len(line) > e.ScreenCols {
+				line = line[0:e.ScreenCols]
+			}
+			buffer = append(buffer, []byte(line)...)
+			buffer = append(buffer, []byte("\x1b[K")...)
 			buffer = append(buffer, []byte("\r\n")...)
 		} else {
-			buffer = append(buffer, []byte(e.Message)...)
+			if y == e.ScreenRows/3 {
+				welcome := fmt.Sprintf("goed editor -- version %s", VERSION)
+				padding := (e.ScreenCols - len(welcome)) / 2
+				buffer = append(buffer, []byte("~")...)
+				for i := 1; i <= padding; i++ {
+					buffer = append(buffer, []byte(" ")...)
+				}
+				buffer = append(buffer, []byte(welcome)...)
+			} else {
+				buffer = append(buffer, []byte("~")...)
+			}
+			buffer = append(buffer, []byte("\x1b[K")...)
+			buffer = append(buffer, []byte("\r\n")...)
 		}
 	}
 	return buffer
@@ -182,22 +242,21 @@ func (e *Editor) MoveCursor(key int) {
 			e.CursorCol--
 		}
 	case ARROW_RIGHT:
-		if e.CursorCol < e.ScreenCols-1 {
-			e.CursorCol++
-		}
+		//	if e.CursorCol < e.ScreenCols-1 {
+		e.CursorCol++
+	//	}
 	case ARROW_UP:
 		if e.CursorRow > 0 {
 			e.CursorRow--
 		}
 	case ARROW_DOWN:
-		if e.CursorRow < e.ScreenRows-1 {
+		if e.CursorRow < len(e.Rows)-1 {
 			e.CursorRow++
 		}
 	}
 }
 
 func main() {
-
 	// put the terminal into raw mode
 	oldState, err := terminal.MakeRaw(0)
 	if err != nil {
@@ -207,6 +266,7 @@ func main() {
 	defer terminal.Restore(0, oldState)
 
 	e := NewEditor()
+	e.ReadFile("goed.go")
 	// input loop
 	for {
 		e.RefreshScreen()
